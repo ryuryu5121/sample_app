e3426cc0d0a2086c413795fffe80edd48cee123d
ryuryu5121 <r.oshima.12.15@gmail.com>
2023-03-29 13:50:15 +0900

    finish chapter-9

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -68,8 +68,7 @@ class InvalidPasswordTest < UsersLogin
   end
 
   test "login with valid email/invalid password" do
-    post login_path, params: { session: { email:    @user.email,
-                                          password: "invalid" } }
+    post login_path, params: { session: { email:    @user.email, password: "invalid" } }
     assert_not is_logged_in?
     assert_template 'sessions/new'
     assert_not flash.empty?
@@ -82,8 +81,7 @@ class ValidLogin < UsersLogin
 
   def setup
     super
-    post login_path, params: { session: { email:    @user.email,
-                                          password: 'password' } }
+    post login_path, params: { session: { email:    @user.email, password: 'password' } }
   end
 end
 
@@ -124,5 +122,26 @@ class LogoutTest < Logout
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "shouled still work after logout in second window" do
+    delete logout_path
+    assert_redirected_to root_url
+  end
+end
+
+class RememberingTest < UsersLogin
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not cookies[:remember_token].blank?
+  end
+
+  test "login without remembering" do
+    # Cookieを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    # Cookieが削除されていることを検証してからログイン
+    log_in_as(@user, remember_me: '0')
+    assert cookies[:remember_token].blank?
   end
 end
