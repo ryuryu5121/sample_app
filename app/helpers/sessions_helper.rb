db4a8ee25f5545036a614f61fcf027c2f8e38357
ryuryu5121 <r.oshima.12.15@gmail.com>
2023-03-31 15:51:02 +0900

    スペースの削除

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -28,9 +28,9 @@ module SessionsHelper
   end
 
   def forget(user)
-      user.forget
-      cookies.delete(:user_id)
-      cookies.delete(:remember_token)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
   end
 
   def log_out
@@ -40,9 +40,9 @@ module SessionsHelper
   end
 
   def remember(user)
-      user.remember
-      cookies.permanent.encrypted[:user_id] = user.id
-      cookies.permanent[:remember_token] = user.remember_token
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
   end
 
   def store_location
